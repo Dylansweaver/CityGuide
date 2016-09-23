145d989a257fdf664d2836846d60f802ab435b62
Dylansweaver <dweaver@e104-m-17.mchenry.edu>
2016-09-23 10:08:19 -0500

    CityGuide

---
+++ app/src/main/java/com/example/dweaver/cityguide/Pier.java
@@ -0,0 +1,14 @@
+package com.example.dweaver.cityguide;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Pier extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pier);
+    }
+
+}
